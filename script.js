b23bdccbb74dc07fc5a37026d4dc0a5f357bb8f2
sidvilaasa <155668346+sidvilaasa@users.noreply.github.com>
2024-07-28 16:00:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var Ai = {
             y: (this.canvas.height / 2) - 35,
             score: 0,
             move: DIRECTION.IDLE,
-            speed: 8
+            speed: 15
         };
     }
 };
